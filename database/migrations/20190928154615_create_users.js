73c669e9144143e661a83d491768719193ba49ae
Daniel Ichiyama <daniel.ichiyama@gmail.com>
2019-09-28 16:06:20 -1000

    adds migration files for user, cards, and priorities

---
+++ database/migrations/20190928154615_create_users.js
@@ -0,0 +1,13 @@
+exports.up = function(knex) {
+  return knex.schema.createTable("users", table => {
+    table.increments();
+    table.string("first_name").notNullable();
+    table.string("last_name").notNullable();
+    table.unique("email").notNullable();
+    table.timestamps(true, true);
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTable("users");
+};
