9875ebdbeb9cbaf3ec822810f6c2dabf24f29a10
Daniel Ichiyama <daniel.ichiyama@gmail.com>
2020-03-19 01:17:26 -1000

    adds 404 error page

--- src/App/App.js
+++ src/App/App.js
@@ -1,10 +1,16 @@
 import React from "react";
 import "./App.scss";
 import LoginPage from "../pages/LoginPage";
+import ErrorPage from "../pages/ErrorPage";
 import BoardsList from "../components/BoardsList";
 import Board from "../components/Board";
 
-import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch
+} from "react-router-dom";
 
 let App = () => {
   const PrivateRoute = ({ component: Component, ...rest }) => {
@@ -23,12 +29,15 @@ let App = () => {
   };
 
   return (
-    <div>
+    <div className="App">
       <Router>
-        <Route path="/login" exact={true} component={LoginPage} />
-        <Route path="/register" exact={true} component={LoginPage} />
-        <PrivateRoute path="/" exact={true} component={BoardsList} />
-        <PrivateRoute path="/board" exact={true} component={Board} />
+        <Switch>
+          <Route path="/login" exact={true} component={LoginPage} />
+          <Route path="/register" exact={true} component={LoginPage} />
+          <PrivateRoute path="/boards" exact={true} component={BoardsList} />
+          <PrivateRoute path="/board" exact={true} component={Board} />
+          <Route component={ErrorPage} />
+        </Switch>
       </Router>
     </div>
   );
